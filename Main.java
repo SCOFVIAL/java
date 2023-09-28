973c0a879e4f7dd60fb47d0c4798d3bc53d4a801
SCOFVIAL <alliuvictor@gmail.com>
2023-09-28 04:25:25 +0100

    initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class Main {
+    public static void main (String[] args){
+        // byte age = 30;        
+        // long viewsCount = 3_123_456_789L;
+        // float price = 10.99F;
+        // char letter = 'A';
+        // boolean isEligible = true;
+        // Date now = new Date();
+        // System.out.println(now);
+//Reference type
+    //     byte x = 1;
+    //     byte y = x;
+    //     x = 2;
+    //     System.out.println(y);
+    }
+}
